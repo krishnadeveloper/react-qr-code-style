ed1d85ed779fd0500a00b8418c4ddc4036ac3975
Krishna Kumar <getkrishnakumar1990@gmail.com>
2021-11-14 16:10:08 +0530

    Bootstrap css import

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './Qrcode';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from "react-redux"
-import store from './store';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-    <App />
-    </Provider>
-    
+   <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
